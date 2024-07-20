015fc7e361d416395596e846fbd46ffe9690521b
Thiago Mena <menathiagx@gmail.com>
2024-07-20 18:37:40 -0300

    I really dont reememeb what i did xD

--- src/commands/utility/avatar.js
+++ src/commands/utility/avatar.js
@@ -1,16 +1,22 @@
-const { SlashCommandBuilder } = require('discord.js');
+import { SlashCommandBuilder } from "discord.js";
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('avatar')
-		.setDescription('Muestra el avatar del usuario.')
-		.addUserOption(option =>
-			option.setName('usuario')
-				.setDescription('Usuario del que quieres mostrar la foto.')
-				.setRequired(true)),
-	category: 'utility',
-	async execute(interaction) {
-		const usuario = interaction.options.getUser('usuario');
-		if (usuario) return interaction.reply(`${usuario.username} avatar: ${usuario.displayAvatarURL()}`);
-	},
+  data: new SlashCommandBuilder()
+    .setName("avatar")
+    .setDescription("Muestra el avatar del usuario.")
+    .addUserOption((option) =>
+      option
+        .setName("usuario")
+        .setDescription("Usuario del que quieres mostrar la foto.")
+        .setRequired(true),
+    ),
+  category: "utility",
+  async execute(interaction) {
+    const usuario = interaction.options.getUser("usuario");
+    if (usuario)
+      return interaction.reply(
+        `${usuario.username} avatar: ${usuario.displayAvatarURL()}`,
+      );
+  },
 };
+
